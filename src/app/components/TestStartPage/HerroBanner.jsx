178209b8572874694ef6a92ffaa2ad7bf69a7eb1
Olexsandr Tepliuk <teolex2017@gmail.com>
2025-04-15 15:49:31 +0300

    add

--- src/app/components/TestStartPage/HerroBanner.jsx
+++ src/app/components/TestStartPage/HerroBanner.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
-import bannerImage from '../../../../public/assets/log.webp'
+import bannerImage from '../../../../public/assets/l.webp'
 
 const HeroBanner = () => {
 	return (
@@ -11,8 +11,8 @@ const HeroBanner = () => {
 				<div className='absolute inset-0 bg-gradient-to-tr from-[#12181d00] via-[#10151a] to-[#141414] z-0 pointer-events-none ' />
 
 				{/* Ліва частина — текст */}
-				<div className='flex flex-col justify-center items-start text-left px-5 sm:px-10 py-6 sm:py-10 lg:w-1/2 z-10 gap-5'>
-					<div className='sm:absolute top-4 left-4 sm:top-6 sm:left-8 z-20  hover:animate-spin '>
+				<div className='flex flex-col justify-center items-start text-left px-5  py-6 sm:py-10 lg:w-1/2 z-10 gap-5 '>
+					<div className='sm:absolute top-4 left-4 sm:top-6 sm:left-8 z-20   '>
 						<motion.p
 							initial={{ opacity: 0, y: -10 }}
 							animate={{ opacity: 1, y: 0 }}
@@ -27,17 +27,17 @@ const HeroBanner = () => {
 						initial={{ opacity: 0, y: -20 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.8 }}
-						className='text-xl sm:text-5xl font-bold text-[#d8b150] max-w-xl leading-tight'
+						className='text-xl sm:text-6xl font-bold text-[#e0a603] max-w-xl leading-tight '
 						style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}
 					>
-						Навчайся з провідними експертами
+						Навчайся з лідерами
 					</motion.h1>
 
 					<motion.p
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.8, delay: 0.2 }}
-						className='text-gray-200 text-lg sm:text-xl max-w-md'
+						className='text-gray-200 text-lg  max-w-md pb-10'
 					>
 						Онлайн-курси, лекції та тренінги в зручному форматі
 					</motion.p>
@@ -48,7 +48,7 @@ const HeroBanner = () => {
 						initial={{ opacity: 0, scale: 0.9 }}
 						animate={{ opacity: 1, scale: 1 }}
 						transition={{ duration: 0.5, delay: 0.4 }}
-						className='px-6 py-3 border border-[#d8b150] rounded-xl text-[#d8b150] hover:bg-[#d8b150] hover:text-black transition font-semibold hidden sm:block hover:shadow-lg'
+						className='px-6 py-3 border border-[#e0a603] rounded-xl text-[#e0a603] hover:bg-[#e0a603] hover:text-black transition font-semibold hidden sm:block hover:shadow-lg'
 					>
 						Переглянути курси
 					</motion.button>
